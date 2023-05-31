c95d2e3bbc5bec541d296d1fb902ddf60a9a82e1
panku-chavan <pankajchavan963@gmail.com>
2023-05-31 19:43:51 +0530

    skeletop loading

--- src/components/carousel/Carousel.jsx
+++ src/components/carousel/Carousel.jsx
@@ -12,18 +12,26 @@ import Img from "../lazyLoadImage/Img";
 import PosterFallback from "../../assets/no-poster.png";
 
 import "./style.scss";
-
+import RatingCircle from "../ratingCircle/RatingCircle";
 
 const Carousel = ({ data, loading }) => {
-
-
     const carouselContainer = useRef();
     const { url } = useSelector((state) => state.home);
     const navigate = useNavigate();
 
-    const navigation = (direction) => {
-
-    }
+    const navigation = (direction) => { };
+    const skItems = () => {
+        return (
+            <div className="skeletonItem">
+                <div className="posterBlock skeleton">
+                    <div className="textBlock">
+                        <div className="title skeleton"></div>
+                        <div className="date skeleton"></div>
+                    </div>
+                </div>
+            </div>
+        );
+    };
     return (
         <div className="carousel">
             <ContentWrapper>
@@ -35,29 +43,40 @@ const Carousel = ({ data, loading }) => {
                     className="carouselRightNav arrow"
                     onClick={() => navigation("right")}
                 />
-                {
-                    !loading ? (
-                        <div className="carouselItems">
-                            {
-                                data?.map((item) => {
-                                    const posterUrl = item.poster_path ? url.poster + item.poster_path : PosterFallback;
-                                    return (
-                                        <div key={item.id} className="carouselItem">
-                                            <div className="posterBlock">
-                                                <Img src={posterUrl} />
-                                            </div>
-                                        </div>
-                                    )
-                                })
-                            }
-                        </div>
-                    ) : (
-                        <span>Loading....</span>
-                    )
-                }
+                {!loading ? (
+                    <div className="carouselItems">
+                        {data?.map((item) => {
+                            const posterUrl = item.poster_path
+                                ? url.poster + item.poster_path
+                                : PosterFallback;
+                            return (
+                                <div key={item.id} className="carouselItem">
+                                    <div className="posterBlock">
+                                        <Img src={posterUrl} />
+                                        <RatingCircle rating={item.vote_average.toFixed(1)} />
+                                    </div>
+                                    <div className="textBlock">
+                                        <span className="title">{item.title || item.name}</span>
+                                        <span className="date">
+                                            {dayjs(item.release_Date).format("MMM D, YYYY")}
+                                        </span>
+                                    </div>
+                                </div>
+                            );
+                        })}
+                    </div>
+                ) : (
+                    <div className="loadingSkeleton">
+                        {skItems()}
+                        {skItems()}
+                        {skItems()}
+                        {skItems()}
+                        {skItems()}
+                    </div>
+                )}
             </ContentWrapper>
         </div>
-    )
-}
+    );
+};
 
-export default Carousel
+export default Carousel;
