e1bbf48f654c8fa9b71ff45ae598f83e93e04d79
panku-chavan <pankajchavan963@gmail.com>
2023-06-02 13:20:02 +0530

    Details Banner complete

--- src/pages/details/Deatils.jsx
+++ src/pages/details/Deatils.jsx
@@ -4,11 +4,12 @@ import useFetch from '../../hooks/useFetch'
 import { useParams } from 'react-router-dom'
 import DetailsBanner from './detailsBanner/DetailsBanner'
 const Deatils = () => {
-    // const params = useParams();
-    // const {data, loading}= useFetch(`/${params.mediaType}/${params.id}`)
+    const params = useParams();
+    const { data, loading } = useFetch(`/${params.mediaType}/${params.id}/videos`)
+    const { data: credits, loading: creditsLoading } = useFetch(`/${params.mediaType}/${params.id}/credits`)
     return (
         <div>
-            <DetailsBanner />
+            <DetailsBanner video={data?.results?.[0]} crew={credits?.crew} />
         </div>
     )
 }
--- src/pages/details/detailsBanner/DetailsBanner.jsx
+++ src/pages/details/detailsBanner/DetailsBanner.jsx
@@ -12,8 +12,11 @@ import Img from "../../../components/lazyLoadImage/Img.jsx";
 import PosterFallback from "../../../assets/no-poster.png";
 import RatingCircle from "../../../components/ratingCircle/RatingCircle";
 import { PlayButton } from "../PlayButton";
+import VideoPopup from "../../../components/videoPopup/VideoPopup";
 
 const DetailsBanner = ({ video, crew }) => {
+    const [show, setShow] = useState(false);
+    const [videoId, setVideoId] = useState(null);
 
     const params = useParams();
     const { data, loading } = useFetch(`/${params.mediaType}/${params.id}`)
@@ -21,6 +24,9 @@ const DetailsBanner = ({ video, crew }) => {
     const { url } = useSelector((state) => state.home);
 
     const _genres = data?.genres?.map((g) => g.id);
+
+    const director = crew?.filter((f) => f.job === "Director");
+    const writer = crew?.filter((f) => f.job === "Screenplay" || f.job === "Story" || f.job === "Writer")
 
     const toHoursAndMinutes = (totalMinutes) => {
         const hours = Math.floor(totalMinutes / 60);
@@ -60,7 +66,11 @@ const DetailsBanner = ({ video, crew }) => {
 
                                         <div className="row">
                                             <RatingCircle rating={data?.vote_average.toFixed()} />
-                                            <div className="playbtn">
+                                            <div className="playbtn" onClick={() => {
+                                                setShow(true)
+                                                setVideoId(video?.key)
+
+                                            }}>
                                                 <PlayButton />
                                                 <span className="text">Watch Trailer</span>
                                             </div>
@@ -116,8 +126,53 @@ const DetailsBanner = ({ video, crew }) => {
                                             }
 
                                         </div>
+                                        {
+                                            director?.length > 0 && (
+                                                <div className="info">
+                                                    <span className="text bold">
+                                                        Director:{" "}
+                                                    </span>
+                                                    <span className="text">
+                                                        {director?.map((d, i) => (
+                                                            <span key={i}>{d.name} {director?.length - 1 !== i && ", "}</span>
+                                                        ))}
+                                                    </span>
+                                                </div>
+                                            )
+                                        }
+
+                                        {
+                                            writer?.length > 0 && (
+                                                <div className="info">
+                                                    <span className="text bold">
+                                                        Writer:{" "}
+                                                    </span>
+                                                    <span className="text">
+                                                        {writer?.map((d, i) => (
+                                                            <span key={i}>{d.name} {writer?.length - 1 !== i && ", "}</span>
+                                                        ))}
+                                                    </span>
+                                                </div>
+                                            )
+                                        }
+
+                                        {
+                                            data?.created_by?.length > 0 && (
+                                                <div className="info">
+                                                    <span className="text bold">
+                                                        Creator:{" "}
+                                                    </span>
+                                                    <span className="text">
+                                                        {data.created_by?.map((d, i) => (
+                                                            <span key={i}>{d.name} {data.created_by?.length - 1 !== i && ", "}</span>
+                                                        ))}
+                                                    </span>
+                                                </div>
+                                            )
+                                        }
                                     </div>
                                 </div>
+                                <VideoPopup show={show} setShow={setShow} videoId={videoId} setVideoId={setVideoId} />
                             </ContentWrapper>
                         </>
                     )}
