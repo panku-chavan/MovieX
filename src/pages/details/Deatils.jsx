e0783772605686ffd1a9d2f4765c48a24adcb50b
panku-chavan <pankajchavan963@gmail.com>
2023-06-02 12:21:07 +0530

    Details page

--- src/pages/details/Deatils.jsx
+++ src/pages/details/Deatils.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
 import "./style.scss"
+import useFetch from '../../hooks/useFetch'
+import { useParams } from 'react-router-dom'
+import DetailsBanner from './detailsBanner/DetailsBanner'
 const Deatils = () => {
+    // const params = useParams();
+    // const {data, loading}= useFetch(`/${params.mediaType}/${params.id}`)
     return (
         <div>
-
+            <DetailsBanner />
         </div>
     )
 }
