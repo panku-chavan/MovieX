1e4d29b1b4bf6fbaeda2d86b73eabe38c610f08c
panku-chavan <pankajchavan963@gmail.com>
2023-06-02 20:34:33 +0530

    trending error solved

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import "./style.scss"
 import HeroBanner from './heroBanner/HeroBanner'
-import Trending from './trending/trending'
+import Trending from './trending/Trending'
 import Popular from './popular/Popular'
 import TopRated from './topRated/TopRated'
 const Home = () => {
